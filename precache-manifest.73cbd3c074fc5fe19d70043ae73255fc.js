b9856408a96f096abe4b3f3486ee29f96a828960
lltim <48389438+lltim@users.noreply.github.com>
2020-04-13 20:44:10 -0400

    Updates

---
+++ precache-manifest.73cbd3c074fc5fe19d70043ae73255fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "684c12db54e300a4d29958ffa9fada56",
+    "url": "/expense-tracker-react/index.html"
+  },
+  {
+    "revision": "e334c10c25e4c3f89947",
+    "url": "/expense-tracker-react/static/css/main.d0e32a8e.chunk.css"
+  },
+  {
+    "revision": "c69cf5be7f2641b55750",
+    "url": "/expense-tracker-react/static/js/2.a177d7c0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/expense-tracker-react/static/js/2.a177d7c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e334c10c25e4c3f89947",
+    "url": "/expense-tracker-react/static/js/main.5521c10e.chunk.js"
+  },
+  {
+    "revision": "75740aaa571aaa47fa4e",
+    "url": "/expense-tracker-react/static/js/runtime-main.4695ed7d.js"
+  }
+]);
